869e5c925dc422e965d2b2ee4dfb326f34ed9cfb
Geraldo157 <gsmecanica76@gmail.com>
2023-03-24 14:19:47 -0300

    Inicio do SQL
    Inicio da conexão com SQL e criação do escopo da pagina de cadastro 2

---
+++ src/main/java/SignUpTwo.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SignUpTwo {
+    
+    SignUpTwo(){
+        
+    }
+    
+    public static void main(String[] ars){
+        
+    new SignUpTwo();
+    
+    }
+}
